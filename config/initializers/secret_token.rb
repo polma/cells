7f8d35c14a1d812d8ad27c0ca370f4e19c54140f
Piotr Olma <perdo.olma@gmail.com>
2013-01-22 00:18:50 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cellstest::Application.config.secret_token = 'e2ef851018567584caf2ec1bcfa0863b336251dd4a5921fbe7dd79cffabc7b11b0e5379c70f4cb801236832f78eb97b48f021236fe0d89ad6c55d59650707744'
